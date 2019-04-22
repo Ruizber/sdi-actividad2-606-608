1f9a3f2a0c512122e8f943846c79a977e3c8929e
Fernando Ruiz Berciano <ruizbolyt@gmail.com>
2019-04-22 13:53:27 +0200

    Cambiado nombre

--- sdi-actividad2-606-608-test/src/main/java/com/uniovi/SdiActividad2606608Application.java
+++ sdi-actividad2-606-608-test/src/main/java/com/uniovi/SdiActividad2606608Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SdiEntrega1606608Application {
+public class SdiActividad2606608Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SdiEntrega1606608Application.class, args);
+		SpringApplication.run(SdiActividad2606608Application.class, args);
 	}
 
 }
